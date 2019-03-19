edf75ee391f658c9e3e74240baa9a578090ce0c4
rob <robertchen234@gmail.com>
2019-03-19 19:48:01 -0400

    codewars highest-and-lowest whats-up-next

---
+++ codewars/whats-up-next.js
@@ -0,0 +1,30 @@
+// Given a sequence of items and a specific item in that sequence, return the item immediately following the item specified. If the item occurs more than once in a sequence, return the item after the first occurence. This should work for a sequence of any type.
+
+// When the item isn't present or nothing follows it, the function should return nil in Clojure and Elixir, Nothing in Haskell, undefined in JavaScript, None in Python.
+
+// nextItem([1, 2, 3, 4, 5, 6, 7], 3) # 4
+// nextItem("testing", "t") # "e"
+
+function nextItem(xs, item) {
+  var found = false;
+  for (var x of xs) {
+    if (found) return x;
+    if (x == item) found = true;
+  }
+  return undefined;
+}
+
+// function nextItem(xs, item) {
+//   // TODO: Implement me
+  
+//   let target = undefined
+  
+//   for (let i = 0; i < xs.length; i++) {
+//     if (xs[i] === item) {
+//       target = xs[i+1]
+//       i = xs.length
+//       return target
+//     }
+//   }
+//   return target
+// }
