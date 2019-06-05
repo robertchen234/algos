9a9c30425d3911ef0eb04d18c5a968f5b64e76ba
rob <robertchen234@gmail.com>
2019-06-05 15:16:14 -0400

    codesignal check palindrome

---
+++ codesignal/check-palindrome.js
@@ -0,0 +1,9 @@
+function checkPalindrome(inputString) {
+  for (let i = 0; i < inputString.length/2; i++) {
+      if (inputString[i] !== inputString[inputString.length - 1 - i]) {
+          return false
+      }
+  }
+  
+  return true
+}
